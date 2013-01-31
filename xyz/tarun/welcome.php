77e8509d14784bfbf9d053b95ff6df1b8cf946d3
SamarMacMini <SamarMacMini@SsamarMacMinis-Mac-mini.local>
2013-01-31 16:55:59 +0530

    main

---
+++ xyz/tarun/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST['fname'], $_POST['lname']; ?>!<p>
+Your Username is <?php echo $_POST['uname']; ?> <p>
+Your Email is <?php echo $_POST['email']; ?>
+
+</body>
+</html>
